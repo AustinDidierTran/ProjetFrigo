b2cda8e02a66cfab88f5279d26e3c1f0a9457f3c
austindidiertran1 <tran.austin.didier@gmail.com>
2015-10-05 16:34:15 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path'),
+    rootPath = path.normalize(__dirname);
+
+app.use(express.static(rootPath));
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+
+app.listen(process.env.PORT);
