932dc75392e7618c3aa0814bd14b967dc91272bb
austindidiertran1 <tran.austin.didier@gmail.com>
2015-11-09 19:35:23 +0000

    Hey, iron collapse works!

--- frigo.js
+++ frigo.js
@@ -1,63 +1,85 @@
+$( document ).ready(function(){
+    
+    init();
+
+    $( "target" ).on( "click", function() {
+        console.log( "A button with the alert class was clicked!" );
+    });
+});
+
+//Show the "Bienvenue" page and hide "Home" page
 var showBienvenue = function(){
     $("#bienvenuePage").css('display','block');
     $("#homePage").css('display','none');
 }
 
+//Show the "Home" page and hide the "Bienvenue" page
 var showHome = function(){
     $("#bienvenuePage").css('display','none');
     $("#homePage").css('display','block');
 }
 
-document.addEventListener('WebComponentsReady', function () {
-    var template = document.querySelector('template[is="dom-bind"]');
-    
-    template.selected = 0;
-    
-    template.status = 'Not granted';
-    template.offlineCode = 'No offline login.';
-    template.userName = 'N/A';
-    template.handleSignIn = function(response) {
-        showHome();
-        console.log("signed in");
-        this.status = 'Signin granted';
-        // console.log('[Aware] Signin Response', response);
-        this.userName = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getName();
-    };
-    template.handleOffline = function(response) {
-        console.log("Handle offline");
-      this.offlineCode = response.detail.code;
-    };
-    template.handleSignOut = function(response) {
-        showBienvenue();
-        console.log("Handle sign out");
-      this.status = 'Signed out';
-      // console.log('[Aware] Signout Response', response);
-      this.userName = 'N/A';
-    };
-    template.disconnect = function() {
-        showBienvenue();
-        console.log("Disconnect");
-      var b = document.querySelector('google-signin');
-      var currentUser = gapi.auth2.getAuthInstance().currentUser.get();
-      if (currentUser) {
-        currentUser.disconnect();
-      }
-      gapi.auth2.getAuthInstance().signOut();
-    };
-    
-    if(gapi && gapi.auth2 && gapi.auth2.getAuthInstance().currentUser.get()){
+//Is called on document ready
+var init = function(){
+    document.addEventListener('WebComponentsReady', function () {
+        var template = document.querySelector('template[is="dom-bind"]');
         
-        var user = gapi.auth2.getAuthInstance().currentUser.get();
-        console.log(user);
+        console.log(template);
         
-        showHome();
-    }else{
-        showBienvenue();
-    }
-    
-});
+        template.selected = 0;
+        
+        template.data = [
+          {"name": "Bob"},
+          {"name": "Tim"},
+          {"name": "Mike"}
+        ];
+        
+        template.status = 'Not granted';
+        template.offlineCode = 'No offline login.';
+        template.userName = 'N/A';
+        template.handleSignIn = function(response) {
+            showHome();
+            console.log("signed in");
+            this.status = 'Signin granted';
+            // console.log('[Aware] Signin Response', response);
+            this.userName = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getName();
+        };
+        template.handleOffline = function(response) {
+            console.log("Handle offline");
+          this.offlineCode = response.detail.code;
+        };
+        template.handleSignOut = function(response) {
+            showBienvenue();
+            console.log("Handle sign out");
+          this.status = 'Signed out';
+          // console.log('[Aware] Signout Response', response);
+          this.userName = 'N/A';
+        };
+        template.disconnect = function() {
+            showBienvenue();
+            console.log("Disconnect");
+          var b = document.querySelector('google-signin');
+          var currentUser = gapi.auth2.getAuthInstance().currentUser.get();
+          if (currentUser) {
+            currentUser.disconnect();
+          }
+          gapi.auth2.getAuthInstance().signOut();
+        };
+        
+        if(gapi && gapi.auth2 && gapi.auth2.getAuthInstance().currentUser.get() && gapi.auth2.getAuthInstance().currentUser.get().El){
+            
+            var user = gapi.auth2.getAuthInstance().currentUser.get();
+            console.log(user);
+            showHome();
+        }else{
+            showBienvenue();
+        }
+        
+    });
+}
 
-showBienvenue();
+
+
 
 /*
 $('google-signin').bind('google-signin-aware-success', function(){
